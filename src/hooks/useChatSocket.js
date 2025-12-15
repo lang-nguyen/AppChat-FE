be249869e3d380e2e78b253d6e5eea6a452385f0
Lang Nguyen <thilang0608@gmail.com>
2025-12-15 13:36:24 +0700

    Add websocket

---
+++ src/hooks/useChatSocket.js
@@ -0,0 +1,55 @@
+import {userEffect, userRef, userState} from 'react';
+
+const WS_URL = 'wss://chat.longapp.site/chat/chat';
+
+export const useChatSocket = ()=>{
+    const socketRef = userRef(null); // luu ket noi
+
+    const [messages, setMessages] = userState([]); // list tin nhan
+    const [isReady, setIsReady] = userState(false); // trang thai ket noi
+
+    userEffect(()=>{
+        // 1. Tao ket noi WebSocket
+        const socket = new WebSocket(WS_URL);
+        socketRef.current = socket;
+
+        // khi ket noi thanh cong
+        socket.onopen = ()=>{
+            console.log('WebSocket da ket noi');
+            setIsReady(true)
+        };
+
+        // 2. Lang nghe tin nhan tu server
+        socket.onmessage = (event)=>{
+            const response = JSON.parse(event.data); // String -> Object
+            console.log('Tin nhan moi:', response);
+        };
+
+        // Khi mat ket noi
+        socket.onclose = ()=>{
+            console.log('WebSocket da ngat ket noi');
+            setIsReady(false);
+        };
+
+        return ()=>{
+            socket.close();
+        };
+    }, []);
+
+    // 3. Ham gui tin nhan
+    const sendData = (eventName, dataPayload)=>{
+        if(socketRef.current && socketRef.current.readyState === WebSocket.OPEN){
+            const payload = {
+                action: "onchat",
+                data: {
+                    event: eventName,
+                    data: dataPayload
+                }
+            };
+            socketRef.current.send(JSON.stringify(payload)); // Object -> String
+        }else{
+            console.error('WebSocket chua san sang de gui tin nhan');
+        }
+    };
+    return {isReady, messages, sendData};
+};
