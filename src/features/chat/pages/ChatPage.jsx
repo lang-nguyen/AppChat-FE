02451b4851b6afa080ffef784a75a289f060049f
Souma18 <22130078@st.hcmuaf.edu.vn>
2025-12-29 19:26:37 +0700

    chat page chuyen sang sai hook

--- src/features/chat/pages/ChatPage.jsx
+++ src/features/chat/pages/ChatPage.jsx
@@ -1,42 +1,22 @@
 import styles from "./ChatPage.module.css";
-import {useSocket} from "../../../app/providers/SocketProvider.jsx";
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {setActiveChat} from "../../../state/chat/chatSlice.js";
 import UserHeader from "../components/sidebar/UserHeader.jsx";
 import SearchBox from "../components/sidebar/SearchBox.jsx";
 import RoomList from "../components/sidebar/RoomList.jsx";
 import ChatPlaceholder from "../components/chatbox/ChatPlaceholder.jsx";
+import {useChatSidebar} from "../hooks/useChatSidebar.js";
 
 const ChatPage = () => {
-    const {actions, isReady} = useSocket();
-    const dispatch = useDispatch();
-    const people = useSelector((s) => s.chat.people);
-    const activeChat = useSelector((s) => s.chat.activeChat);
-
-    useEffect(() => {
-        if (isReady) actions.getUserList();
-    }, [isReady, actions]);
-
-    const rooms = people.map((x) => ({
-        key: `${x.type}:${x.name}`,
-        name: x.name,
-        type: x.type, // 0 people, 1 group
-        badge: x.type === 1 ? "Group" : "People",
-        lastMessage: x.actionTime ?? "",
-        active: activeChat?.name === x.name && activeChat?.type === x.type,
-    }));
+    const {title, rooms, selectRoom} = useChatSidebar();
 
     return (
         <div className={styles.page}>
             <div className={styles["chat-container"]}>
                 <div className={styles["chat-sidebar"]}>
-                    <UserHeader name="Tên người dùng" onAdd={() => { /* TODO: create group later */
-                    }}/>
+                    <UserHeader name={title} onAdd={() => { /* TODO: create group later */ }}/>
                     <SearchBox/>
                     <RoomList
                         rooms={rooms}
-                        onSelect={(r) => dispatch(setActiveChat({name: r.name, type: r.type}))}
+                        onSelect={selectRoom}
                     />
                 </div>
                 <div className={styles["chat-main"]}>
