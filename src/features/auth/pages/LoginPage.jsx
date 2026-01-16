ca37a569dfaf2befc8ea7f310a3349733eaa7938
Lang Nguyen <thilang0608@gmail.com>
2026-01-16 17:41:57 +0700

    Import Iridescence style for login and register page

--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import Card from '../../../shared/components/Card.jsx';
 import LoginForm from '../login/LoginForm.jsx';
 import styles from './AuthLayout.module.css';
-
+import Iridescence from '../../../shared/components/Iridescence.jsx';
 const LoginPage = () => {
     // 1. Lấy user từ Redux
     const user = useSelector(state => state.auth.user);
@@ -28,20 +28,31 @@ const LoginPage = () => {
     }, [user, navigate]);
 
     return (
-        <div className={styles.page}>
-            <Card
-                style={{
-                    backgroundColor: 'rgba(255, 255, 255, 0.55)',
-                    cursor: 'default',
-                    backdropFilter: 'blur(8px)',
-                    WebkitBackdropFilter: 'blur(8px)',
-                    border: '2px solid rgba(251, 101, 31, 0.85)',
-                    boxShadow: '0 8px 24px rgba(228, 27, 27, 0.37)',
-                    padding: 40
-                }}
-            >
-                <LoginForm />
-            </Card>
+        <div className={styles.page} style={{ position: 'relative', overflow: 'hidden' }}>
+            <div style={{ position: 'absolute', inset: 0, zIndex: 0 }}>
+                <Iridescence
+                    color={[1, 1, 1]}
+                    mouseReact={false}
+                    amplitude={0.1}
+                    speed={1.0}
+                />
+            </div>
+
+            <div style={{ position: 'relative', zIndex: 1 }}>
+                <Card
+                    style={{
+                        backgroundColor: 'rgba(255, 255, 255, 0.55)',
+                        cursor: 'default',
+                        backdropFilter: 'blur(8px)',
+                        WebkitBackdropFilter: 'blur(8px)',
+                        border: '2px solid rgba(251, 101, 31, 0.85)',
+                        boxShadow: '0 8px 24px rgba(228, 27, 27, 0.37)',
+                        padding: 40
+                    }}
+                >
+                    <LoginForm />
+                </Card>
+            </div>
         </div>
     );
 };
